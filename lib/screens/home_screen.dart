50f2c5a65a884c5b173048a3aba359b2e2d1bac9
manasvi073 <manasvimunjani07@gmail.com>
2025-05-12 17:26:56 +0530

    commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return const Scaffold(
       body: Text('Home'),
     );
   }
