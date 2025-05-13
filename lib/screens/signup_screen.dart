93eb7192fdccbab4fbe5c8fcbc519f3cecf9f7f1
manasvi073 <manasvimunjani07@gmail.com>
2025-05-13 16:53:48 +0530

    commit

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:instagram_clone/constant/appImage_const.dart';
+import 'package:instagram_clone/constant/appimage_const.dart';
 import 'package:instagram_clone/constant/appcolor_const.dart';
 import 'package:instagram_clone/controller/home_controller.dart';
 import 'package:instagram_clone/screens/login_screen.dart';
@@ -15,152 +15,194 @@ class SignupScreen extends StatelessWidget {
     final formkey = GlobalKey<FormState>();
     final HomeController homeController = Get.put(HomeController());
     return Scaffold(
-      backgroundColor: AppColorConst.appBlack,
       body: Form(
         key: formkey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                AppImageConst.appInstagramText,
-                height: 35,
-                width: 120,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                AppImageConst.appBackground2,
+                fit: BoxFit.fill,
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                style:
-                    const TextStyle(color: AppColorConst.appGray, fontSize: 14),
-                decoration: InputDecoration(
-                  hintText: 'Name...',
-                  hintStyle: const TextStyle(
-                      color: AppColorConst.appGray, fontSize: 12),
-                  suffixIcon: const Icon(Icons.account_box_outlined,
-                      color: AppColorConst.appGray, size: 20),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(color: AppColorConst.appGray),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                        color: AppColorConst.appBlue, width: 1.5),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(color: AppColorConst.appGray),
-                  ),
-                ),
-                validator: (value) =>
-                    emptyValidation(value, 'Please enter your name'),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                style:
-                    const TextStyle(color: AppColorConst.appGray, fontSize: 14),
-                decoration: InputDecoration(
-                  hintText: 'Email...',
-                  hintStyle: const TextStyle(
-                      color: AppColorConst.appGray, fontSize: 12),
-                  suffixIcon: const Icon(Icons.email_outlined,
-                      color: AppColorConst.appGray, size: 20),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(color: AppColorConst.appGray),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                        color: AppColorConst.appBlue, width: 1.5),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(color: AppColorConst.appGray),
-                  ),
-                ),
-                validator: emailValidation,
-              ),
-              const SizedBox(height: 20),
-              Obx(
-                () => TextFormField(
-                  style: const TextStyle(
-                      color: AppColorConst.appGray, fontSize: 14),
-                  obscureText: homeController.obscureText.value,
-                  decoration: InputDecoration(
-                    hintText: 'Password...',
-                    hintStyle: const TextStyle(
-                        color: AppColorConst.appGray, fontSize: 12),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide:
-                          const BorderSide(color: AppColorConst.appGray),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: const BorderSide(
-                          color: AppColorConst.appBlue, width: 1.5),
-                    ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide:
-                            const BorderSide(color: AppColorConst.appGray)),
-                    suffixIcon: InkWell(
-                      onTap: homeController.show,
-                      child: Icon(
-                        homeController.obscureText.value
-                            ? Icons.visibility_off_outlined
-                            : Icons.visibility_outlined,
-                        color: AppColorConst.appGray,
-                        size: 20,
-                      ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () => Get.back(),
+                    child: const Icon(
+                      Icons.arrow_back,
+                      size: 20,
+                      color: AppColorConst.appWhite,
                     ),
                   ),
-                  validator: passwordValidation,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                height: 45,
-                decoration: BoxDecoration(
-                    color: AppColorConst.appWhite,
-                    borderRadius: BorderRadius.circular(20)),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Sign Up',
-                    style:
-                        TextStyle(color: AppColorConst.appGray, fontSize: 15),
+                  const SizedBox(height: 30),
+                  const Text(
+                    'Get Started',
+                    style: TextStyle(
+                        color: AppColorConst.appWhite,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600),
                   ),
-                ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    'Please enter your detail to create an account ',
+                    style: TextStyle(
+                        color: AppColorConst.appWhite,
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              RichText(
-                text: TextSpan(
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 60, left: 25, right: 25),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    const TextSpan(
-                      text: "Already have an account? ",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    TextSpan(
-                      text: 'Login',
+                    TextFormField(
                       style: const TextStyle(
-                        color: AppColorConst.appBlue,
-                        decoration: TextDecoration.underline,
+                          color: AppColorConst.appGray, fontSize: 14),
+                      decoration: InputDecoration(
+                        hintText: 'Name...',
+                        hintStyle: const TextStyle(
+                            color: AppColorConst.appGray, fontSize: 12),
+                        suffixIcon: const Icon(Icons.account_box_outlined,
+                            color: AppColorConst.appGray, size: 20),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide:
+                              const BorderSide(color: AppColorConst.appGray),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                              color: AppColorConst.appBlue, width: 1.5),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide:
+                              const BorderSide(color: AppColorConst.appGray),
+                        ),
                       ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Get.to(
-                                () => const LoginScreen(),
-                          );
-                        },
+                      validator: (value) =>
+                          emptyValidation(value, 'Please enter your name'),
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      style: const TextStyle(
+                          color: AppColorConst.appGray, fontSize: 14),
+                      decoration: InputDecoration(
+                        hintText: 'Email...',
+                        hintStyle: const TextStyle(
+                            color: AppColorConst.appGray, fontSize: 12),
+                        suffixIcon: const Icon(Icons.email_outlined,
+                            color: AppColorConst.appGray, size: 20),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide:
+                              const BorderSide(color: AppColorConst.appGray),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                              color: AppColorConst.appBlue, width: 1.5),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide:
+                              const BorderSide(color: AppColorConst.appGray),
+                        ),
+                      ),
+                      validator: emailValidation,
+                    ),
+                    const SizedBox(height: 20),
+                    Obx(
+                      () => TextFormField(
+                        style: const TextStyle(
+                            color: AppColorConst.appGray, fontSize: 14),
+                        obscureText: homeController.obscureText.value,
+                        decoration: InputDecoration(
+                          hintText: 'Password...',
+                          hintStyle: const TextStyle(
+                              color: AppColorConst.appGray, fontSize: 12),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide:
+                                const BorderSide(color: AppColorConst.appGray),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: const BorderSide(
+                                color: AppColorConst.appBlue, width: 1.5),
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                  color: AppColorConst.appGray)),
+                          suffixIcon: InkWell(
+                            onTap: homeController.show,
+                            child: Icon(
+                              homeController.obscureText.value
+                                  ? Icons.visibility_off_outlined
+                                  : Icons.visibility_outlined,
+                              color: AppColorConst.appGray,
+                              size: 20,
+                            ),
+                          ),
+                        ),
+                        validator: passwordValidation,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      width: double.infinity,
+                      height: 45,
+                      decoration: BoxDecoration(
+                          color: AppColorConst.appDarkBlue,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              color: AppColorConst.appWhite, fontSize: 15),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: "Already have an account? ",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          TextSpan(
+                            text: 'Login',
+                            style: const TextStyle(
+                              color: AppColorConst.appBlue,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Get.to(
+                                  () => const LoginScreen(),
+                                );
+                              },
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
