a38a12e8cd314d0870231efe40791dd0b925f1c3
manasvi073 <manasvimunjani07@gmail.com>
2025-05-14 18:46:16 +0530

    commit

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/constant/appcolor_const.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Search Screen');
+    return const Text('Search Screen',style: TextStyle(color: AppColorConst.appWhite),);
   }
 }
