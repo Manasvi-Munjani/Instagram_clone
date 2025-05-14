132e448713a87ffc582280a59579c9dd8c275a34
manasvi073 <manasvimunjani07@gmail.com>
2025-05-14 18:56:12 +0530

    commit

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -6,6 +6,9 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Search Screen',style: TextStyle(color: AppColorConst.appWhite),);
+    return const Text(
+      'Search Screen',
+      style: TextStyle(color: AppColorConst.appWhite),
+    );
   }
 }
