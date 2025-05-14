f7e2450a85ee861b72c9bfe676fbef68ad853392
manasvi073 <manasvimunjani07@gmail.com>
2025-05-14 18:47:15 +0530

    commit

--- lib/screens/reels_screen.dart
+++ lib/screens/reels_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/constant/appcolor_const.dart';
 
 class ReelsScreen extends StatelessWidget {
   const ReelsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Reels Screen');
+    return const Text('Reels Screen',style: TextStyle(color: AppColorConst.appWhite),);
   }
 }
