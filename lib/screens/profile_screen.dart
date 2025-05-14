ce179ed260d93115db92673b7bae42fc81e82f4c
manasvi073 <manasvimunjani07@gmail.com>
2025-05-14 18:55:52 +0530

    commit

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -6,6 +6,9 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Profile Screen',style: TextStyle(color: AppColorConst.appWhite),);
+    return const Text(
+      'Profile Screen',
+      style: TextStyle(color: AppColorConst.appWhite),
+    );
   }
 }
