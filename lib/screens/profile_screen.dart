701cec41789931b8cbefcdc4337edc18252b9a2c
manasvi073 <manasvimunjani07@gmail.com>
2025-05-14 18:46:43 +0530

    commit

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/constant/appcolor_const.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Profile Screen');
+    return const Text('Profile Screen',style: TextStyle(color: AppColorConst.appWhite),);
   }
 }
