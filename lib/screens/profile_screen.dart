55f77225d684f5aec707799f46e7962c34c01fae
manasvi073 <manasvimunjani07@gmail.com>
2025-05-15 14:57:58 +0530

    commit

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/constant/appImage_const.dart';
 import 'package:instagram_clone/constant/appcolor_const.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -6,9 +7,142 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Profile Screen',
-      style: TextStyle(color: AppColorConst.appWhite),
+    return Scaffold(
+      backgroundColor: AppColorConst.appBlack,
+      body: _profileData(),
+    );
+  }
+
+  Widget _profileData() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(13),
+            child: Row(
+              children: [
+                Text(
+                  'john_scott',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w700,
+                    color: AppColorConst.appWhite,
+                  ),
+                ),
+                Spacer(),
+                Icon(
+                  Icons.add_box_outlined,
+                  color: AppColorConst.appWhite,
+                ),
+                SizedBox(width: 15),
+                Icon(
+                  Icons.menu,
+                  color: AppColorConst.appWhite,
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: 70,
+                    height: 70,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: ClipOval(
+                      child: Image.asset(AppImageConst.appDpImage),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      width: 22,
+                      height: 22,
+                      decoration: BoxDecoration(
+                        color: AppColorConst.appBlue,
+                        shape: BoxShape.circle,
+                        border:
+                            Border.all(color: AppColorConst.appBlack, width: 2),
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        size: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Column(
+                children: [
+                  Text(
+                    '5',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17,
+                    ),
+                  ),
+                  Text(
+                    'Post',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              const Column(
+                children: [
+                  Text(
+                    '1000',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17,
+                    ),
+                  ),
+                  Text(
+                    'Followers',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              const Column(
+                children: [
+                  Text(
+                    '500',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17,
+                    ),
+                  ),
+                  Text(
+                    'Following',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
