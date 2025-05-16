482ce0959c7bb8dec994f283c1de230a6e1fe3f0
manasvi073 <manasvimunjani07@gmail.com>
2025-05-16 15:29:06 +0530

    commit

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -8,24 +8,32 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColorConst.appBlack,
-      body: TextField(
-          decoration: InputDecoration(
-        filled: true,
-        fillColor: AppColorConst.appSearchBlack,
-        prefixIcon: const Icon(
-          Icons.search,
-          color: AppColorConst.appLightGray,
-        ),
-        hintText: 'Search',
-        hintStyle: const TextStyle(
-            color: AppColorConst.appLightGray,
-            fontSize: 17,
-            fontWeight: FontWeight.w400),
-        border: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.circular(8),
-        ),
-      )),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 20),
+        child: TextField(
+            style: TextStyle(
+              color: AppColorConst.appLightGray,
+              fontWeight: FontWeight.w400,
+              fontSize: 17,
+            ),
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: AppColorConst.appSearchBlack,
+              prefixIcon: const Icon(
+                Icons.search,
+                color: AppColorConst.appLightGray,
+              ),
+              hintText: 'Search',
+              hintStyle: const TextStyle(
+                  color: AppColorConst.appLightGray,
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            )),
+      ),
     );
   }
 }
