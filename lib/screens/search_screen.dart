0818572a7d6304f98f9af60c2580e8c6ceb25b7c
manasvi073 <manasvimunjani07@gmail.com>
2025-05-16 16:16:12 +0530

    commit

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_clone/constant/appcolor_const.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -8,33 +9,72 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColorConst.appBlack,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-        child: TextField(
-          style: const TextStyle(
-            color: AppColorConst.appLightGray,
-            fontWeight: FontWeight.w400,
-            fontSize: 17,
-          ),
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: AppColorConst.appSearchBlack,
-            prefixIcon: const Icon(
-              Icons.search,
+      body: Column(
+        children: [
+          TextField(
+            style: const TextStyle(
               color: AppColorConst.appLightGray,
+              fontWeight: FontWeight.w400,
+              fontSize: 17,
             ),
-            hintText: 'Search',
-            hintStyle: const TextStyle(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: AppColorConst.appSearchBlack,
+              prefixIcon: const Icon(
+                Icons.search,
                 color: AppColorConst.appLightGray,
-                fontSize: 17,
-                fontWeight: FontWeight.w400),
-            border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(8),
+              ),
+              hintText: 'Search',
+              hintStyle: const TextStyle(
+                  color: AppColorConst.appLightGray,
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(8),
+              ),
             ),
           ),
-        ),
+          const SizedBox(height: 10),
+          Expanded(child: _postData()),
+        ],
       ),
+    );
+  }
+
+  Widget _postData() {
+    final List<String> postImages = [
+      'assets/images/nature.jpg',
+      'assets/images/post1.png',
+      'assets/images/nature2.png',
+      'assets/images/book.jpg',
+      'assets/images/post2.jpg',
+      'assets/images/book2.jpg',
+      'assets/images/doraemon.jpg',
+      'assets/images/flower.jpg',
+      'assets/images/nature.jpg',
+      'assets/images/post1.png',
+      'assets/images/nature2.png',
+      'assets/images/book.jpg',
+      'assets/images/post2.jpg',
+      'assets/images/book2.jpg',
+      'assets/images/doraemon.jpg',
+      'assets/images/flower.jpg',
+    ];
+
+    return MasonryGridView.count(
+      crossAxisCount: 3,
+      mainAxisSpacing: 3,
+      crossAxisSpacing:3,
+      itemCount: postImages.length,
+      itemBuilder: (context, index) {
+        return ClipRRect(
+          child: Image.asset(
+            postImages[index],
+            fit: BoxFit.cover,
+          ),
+        );
+      },
     );
   }
 }
