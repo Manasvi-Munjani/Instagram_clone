4834d2f7783a9512c2f0c209bf9e10ff059bb0c2
manasvi073 <manasvimunjani07@gmail.com>
2025-05-19 11:58:02 +0530

    commit

--- lib/screens/reels_screen.dart
+++ lib/screens/reels_screen.dart
@@ -6,9 +6,57 @@ class ReelsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Reels Screen',
-      style: TextStyle(color: AppColorConst.appWhite),
+    return const Scaffold(
+      backgroundColor: AppColorConst.appBlack,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Reels',
+                  style: TextStyle(
+                    color: AppColorConst.appWhite,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Spacer(),
+                Icon(
+                  Icons.camera_alt_outlined,
+                  color: AppColorConst.appWhite,
+                )
+              ],
+            ),
+            Icon(
+              Icons.favorite_outline_rounded,
+              color: AppColorConst.appWhite,
+            ),
+            Text(
+              '31.2k',
+              style: TextStyle(color: AppColorConst.appWhite),
+            ),
+            Icon(
+              Icons.mode_comment_outlined,
+              color: AppColorConst.appWhite,
+            ),
+            Text(
+              '510',
+              style: TextStyle(color: AppColorConst.appWhite),
+            ),
+            Icon(
+              Icons.send,
+              color: AppColorConst.appWhite,
+            ),
+            Icon(
+              Icons.more_horiz,
+              color: AppColorConst.appWhite,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
