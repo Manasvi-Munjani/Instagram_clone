9af685d8737908b46fdb63115e04e4ed8d9efc51
manasvi073 <manasvimunjani07@gmail.com>
2025-05-19 18:32:56 +0530

    commit

--- lib/screens/audio_screen.dart
+++ lib/screens/audio_screen.dart
@@ -6,33 +6,49 @@ class AudioScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: AppColorConst.appBlack,
-      body: const Row(
-        children: [
-          Icon(
-            Icons.arrow_back,
-            color: AppColorConst.appWhite,
-          ),
-          Text(
-            'Audio',
-            style: TextStyle(
-              color: AppColorConst.appWhite,
-              fontWeight: FontWeight.w700,
-              fontSize: 18,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.arrow_back,
+                  color: AppColorConst.appWhite,
+                ),
+                Text(
+                  'Audio',
+                  style: TextStyle(
+                    color: AppColorConst.appWhite,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 18,
+                  ),
+                ),
+                Spacer(),
+                Icon(
+                  Icons.bookmark_border,
+                  color: AppColorConst.appWhite,
+                ),
+                SizedBox(width: 5),
+                Icon(
+                  Icons.more_vert_outlined,
+                  color: AppColorConst.appWhite,
+                )
+              ],
             ),
-          ),
-          Spacer(),
-          Icon(
-            Icons.bookmark_border,
-            color: AppColorConst.appWhite,
-          ),
-          SizedBox(width: 5),
-          Icon(
-            Icons.more_vert_outlined,
-            color: AppColorConst.appWhite,
-          )
-        ],
+            Text(
+              'Origin audio',
+              style: TextStyle(
+                color: AppColorConst.appOffWhite,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
