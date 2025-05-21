6d57b6c04e6cce6f2c4b9446b19c9f758ca71fc2
manasvi073 <manasvimunjani07@gmail.com>
2025-05-21 10:45:38 +0530

    commit

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -13,57 +13,60 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final HomeController homeController = Get.put(HomeController());
     final user = homeController.userModel.value;
-    return Scaffold(
-      backgroundColor: AppColorConst.appBlack,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              child: Row(
-                children: [
-                  Text(
-                    // 'john_scott',
-                    user!.username,
-
-                    style: const TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.w700,
-                      color: AppColorConst.appWhite,
-                    ),
-                  ),
-                  const Spacer(),
-                  const Icon(
-                    Icons.add_box_outlined,
-                    color: AppColorConst.appWhite,
-                    size: 28,
-                  ),
-                  const SizedBox(width: 15),
-                  const Icon(
-                    Icons.menu,
-                    color: AppColorConst.appWhite,
-                    size: 28,
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
+    return Obx(
+      () => Scaffold(
+        backgroundColor: AppColorConst.appBlack,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                child: Row(
                   children: [
-                    _profileData(),
-                    const SizedBox(height: 20),
-                    _storyHighlights(),
-                    const SizedBox(height: 15),
-                    _postIcon(),
-                    const SizedBox(height: 15),
-                    _postGrid(),
-                    const SizedBox(height: 15),
+                    Text(
+                      // 'john_scott',
+                      user!.username,
+
+                      style: const TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w700,
+                        color: AppColorConst.appWhite,
+                      ),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.add_box_outlined,
+                      color: AppColorConst.appWhite,
+                      size: 28,
+                    ),
+                    const SizedBox(width: 15),
+                    const Icon(
+                      Icons.menu,
+                      color: AppColorConst.appWhite,
+                      size: 28,
+                    )
                   ],
                 ),
               ),
-            ),
-          ],
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      _profileData(),
+                      const SizedBox(height: 20),
+                      _storyHighlights(),
+                      const SizedBox(height: 15),
+                      _postIcon(),
+                      const SizedBox(height: 15),
+                      _postGrid(),
+                      const SizedBox(height: 15),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -73,181 +76,179 @@ class ProfileScreen extends StatelessWidget {
     final HomeController homeController = Get.put(HomeController());
     final user = homeController.userModel.value;
 
-    return Obx(
-      () => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      width: 70,
-                      height: 70,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                      ),
-                      child: ClipOval(
-                        child: Image.asset(
-                          AppImageConst.appDpImage,
-                          fit: BoxFit.cover,
-                        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: 70,
+                    height: 70,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: ClipOval(
+                      child: Image.asset(
+                        AppImageConst.appDpImage,
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: Container(
-                        width: 22,
-                        height: 22,
-                        decoration: BoxDecoration(
-                          color: AppColorConst.appBlue,
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                              color: AppColorConst.appBlack, width: 2),
-                        ),
-                        child: const Icon(
-                          Icons.add,
-                          size: 18,
-                          color: AppColorConst.appWhite,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const Column(
-                  children: [
-                    Text(
-                      '5',
-                      style: TextStyle(
-                        color: AppColorConst.appWhite,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17,
-                      ),
-                    ),
-                    Text(
-                      'Post',
-                      style: TextStyle(
-                        color: AppColorConst.appWhite,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-                const Column(
-                  children: [
-                    Text(
-                      '1000',
-                      style: TextStyle(
-                        color: AppColorConst.appWhite,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17,
-                      ),
-                    ),
-                    Text(
-                      'Followers',
-                      style: TextStyle(
-                        color: AppColorConst.appWhite,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-                const Column(
-                  children: [
-                    Text(
-                      '500',
-                      style: TextStyle(
-                        color: AppColorConst.appWhite,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17,
-                      ),
-                    ),
-                    Text(
-                      'Following',
-                      style: TextStyle(
-                        color: AppColorConst.appWhite,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 17),
-            Text(
-              // 'Jonathan Scott',
-              user!.username,
-
-              style: const TextStyle(
-                color: AppColorConst.appWhite,
-                fontWeight: FontWeight.w600,
-                fontSize: 16,
-              ),
-            ),
-            const SizedBox(height: 4),
-            const Text(
-              'Creative/Artistic',
-              style: TextStyle(
-                color: AppColorConst.appWhite,
-                fontSize: 14,
-              ),
-            ),
-            const SizedBox(height: 4),
-            const Text(
-              'www.johnscott.com',
-              style: TextStyle(
-                color: AppColorConst.appWhite,
-                fontSize: 14,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: GestureDetector(
-                    onTap: () => Get.off(() => const EditProfile()),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
                     child: Container(
-                      height: 40,
+                      width: 22,
+                      height: 22,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: AppColorConst.appLightBlack),
+                        color: AppColorConst.appBlue,
+                        shape: BoxShape.circle,
+                        border:
+                            Border.all(color: AppColorConst.appBlack, width: 2),
                       ),
-                      child: const Center(
-                        child: Text(
-                          'Edit Profile',
-                          style: TextStyle(color: AppColorConst.appWhite),
-                        ),
+                      child: const Icon(
+                        Icons.add,
+                        size: 18,
+                        color: AppColorConst.appWhite,
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 10),
-                Container(
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: AppColorConst.appLightBlack),
-                  ),
-                  child: const Center(
-                    child: Icon(
-                      Icons.keyboard_arrow_down_outlined,
+                ],
+              ),
+              const Column(
+                children: [
+                  Text(
+                    '5',
+                    style: TextStyle(
                       color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17,
+                    ),
+                  ),
+                  Text(
+                    'Post',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              const Column(
+                children: [
+                  Text(
+                    '1000',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17,
+                    ),
+                  ),
+                  Text(
+                    'Followers',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              const Column(
+                children: [
+                  Text(
+                    '500',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17,
+                    ),
+                  ),
+                  Text(
+                    'Following',
+                    style: TextStyle(
+                      color: AppColorConst.appWhite,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 17),
+          Text(
+            // 'Jonathan Scott',
+            user!.username,
+
+            style: const TextStyle(
+              color: AppColorConst.appWhite,
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 4),
+          const Text(
+            'Creative/Artistic',
+            style: TextStyle(
+              color: AppColorConst.appWhite,
+              fontSize: 14,
+            ),
+          ),
+          const SizedBox(height: 4),
+          const Text(
+            'www.johnscott.com',
+            style: TextStyle(
+              color: AppColorConst.appWhite,
+              fontSize: 14,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: GestureDetector(
+                  onTap: () => Get.off(() => const EditProfile()),
+                  child: Container(
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: AppColorConst.appLightBlack),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Edit Profile',
+                        style: TextStyle(color: AppColorConst.appWhite),
+                      ),
                     ),
                   ),
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: AppColorConst.appLightBlack),
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.keyboard_arrow_down_outlined,
+                    color: AppColorConst.appWhite,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
