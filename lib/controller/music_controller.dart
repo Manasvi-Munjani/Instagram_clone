4b138219a002f160869b4413a6dd3a8eb9928f2e
manasvi073 <manasvimunjani07@gmail.com>
2025-05-21 18:30:51 +0530

    commit

--- lib/controller/music_controller.dart
+++ lib/controller/music_controller.dart
@@ -1,4 +1,4 @@
-import 'package:get/get.dart';
+/*import 'package:get/get.dart';
 import 'package:just_audio/just_audio.dart';
 
 class MusicController extends GetxController{
@@ -18,6 +18,47 @@ class MusicController extends GetxController{
       await player.play();
     }
     isPlaying.toggle();
+  }
+
+  @override
+  void onClose() {
+    player.dispose();
+    super.onClose();
+  }
+}*/
+
+import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:file_picker/file_picker.dart';
+
+class MusicController extends GetxController {
+  final AudioPlayer player = AudioPlayer();
+  var isPlaying = false.obs;
+  var currentFile = ''.obs;
+
+  Future<void> pickAudioFile() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+    );
+    if (result != null && result.files.single.path != null) {
+      currentFile.value = result.files.single.name;
+      await player.setFilePath(result.files.single.path!);
+    }
+  }
+
+  void playAudio() async {
+    await player.play();
+    isPlaying.value = true;
+  }
+
+  void pauseAudio() async {
+    await player.pause();
+    isPlaying.value = false;
+  }
+
+  void stopAudio() async {
+    await player.stop();
+    isPlaying.value = false;
   }
 
   @override
