108532c233a7a7263d474570765930c0d16096c0
manasvi073 <manasvimunjani07@gmail.com>
2025-05-22 16:41:16 +0530

    commit

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,28 +1,3 @@
-/*
-class UserModel {
-  String userid;
-  String username;
-  String email;
-  DateTime? time;
-
-  UserModel({
-    required this.userid,
-    required this.username,
-    required this.email,
-    this.time,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'userid': userid,
-      'username': username,
-      'email': email,
-      'time': time,
-    };
-  }
-}
-*/
-
 class UserModel {
   final String userid;
   final String username;
