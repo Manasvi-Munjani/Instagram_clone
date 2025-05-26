1e38d59a9f0e2fc47b5af6d7b295f3b9b59357f9
manasvi073 <manasvimunjani07@gmail.com>
2025-05-26 18:28:32 +0530

    commit

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/constant/appcolor_const.dart';
+import 'package:instagram_clone/validation/app_validation.dart';
 
 class AddPostScreen extends StatelessWidget {
   const AddPostScreen({super.key});
@@ -15,7 +16,7 @@ class AddPostScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Add Post',
               style: TextStyle(
                 color: AppColorConst.appWhite,
@@ -23,25 +24,47 @@ class AddPostScreen extends StatelessWidget {
                 fontWeight: FontWeight.w600,
               ),
             ),
-            SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: 'Add Caption',
-                  hintStyle: TextStyle(
-                      color: AppColorConst.appGray,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 14),
-                  border: OutlineInputBorder()),
+            const SizedBox(height: 15),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Add Caption',
+                hintStyle: TextStyle(
+                    color: AppColorConst.appGray,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14),
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) =>
+                  emptyValidation(value, 'Please add Caption'),
             ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
                 hintText: 'Add description',
                 hintStyle: TextStyle(
                     color: AppColorConst.appGray,
                     fontWeight: FontWeight.w500,
                     fontSize: 14),
                 border: OutlineInputBorder(),
+              ),
+              validator: (value) =>
+                  emptyValidation(value, 'Please add description'),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              height: 45,
+              decoration: BoxDecoration(
+                  color: AppColorConst.appDarkBlue,
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  if (formkey.currentState!.validate()) {}
+                },
+                child: const Text(
+                  'Add Post',
+                  style: TextStyle(color: AppColorConst.appWhite, fontSize: 15),
+                ),
               ),
             ),
           ],
