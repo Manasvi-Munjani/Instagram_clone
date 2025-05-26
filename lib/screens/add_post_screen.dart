32e1862f97d21a0d12df76f199b0487fb72fd7c7
manasvi073 <manasvimunjani07@gmail.com>
2025-05-26 18:04:20 +0530

    commit

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/constant/appcolor_const.dart';
 
 class AddPostScreen extends StatelessWidget {
   const AddPostScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Add Post'),
+    return const Scaffold(
+      backgroundColor: AppColorConst.appBlack,
+      body: Column(
+        children: [
+          Text(
+            'Add Post',
+            style: TextStyle(
+              color: AppColorConst.appWhite,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+
+          TextField()
+        ],
+      ),
     );
   }
 }
