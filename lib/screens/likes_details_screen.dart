0bf780c01a18f1b7d5cf30ae360c9d6461e2abbe
manasvi073 <manasvimunjani07@gmail.com>
2025-06-05 12:49:22 +0530

    -

--- lib/screens/likes_details_screen.dart
+++ lib/screens/likes_details_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:get/get_navigation/get_navigation.dart';
